499668b991d4fa16077f2aade3dac2ae5837a3b9
alhelguera <alhelguera98@gmail.com>
2026-06-02 11:37:29 +0200

    pep versioning

--- modules/shared/shared/__init__.py
+++ modules/shared/shared/__init__.py
@@ -1,7 +1,7 @@
 """Shared utilities for wis2downloader modules."""
 
 # Version of the shared utilities. This can be used by other modules to ensure compatibility.
-__version__ = 'beta 1.0'
+__version__ = '1.0b1'
 
 from .redis_client import get_redis_client
 from .logging import setup_logging
